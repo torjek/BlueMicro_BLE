60d027eee329edad27c80b586cb7fb2f5d3a5ac7
torjek <ntorjek@gmail.com>
2022-04-16 21:53:41 +0200

    swap

--- firmware/keyboards/Malna_V3_old/config/single/keyboard_config.h
+++ firmware/keyboards/Malna_V3_old/config/single/keyboard_config.h
@@ -20,48 +20,46 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 #ifndef KEYBOARD_CONFIG_H
 #define KEYBOARD_CONFIG_H
 #include "hardware_config.h"
-#define KEYBOARD_SIDE MASTER
-// CHANGE THIS FOR THE KEYBOARD TO MATCH WHAT IS BEING FLASHED. OPTIONS: LEFT  RIGHT  MASTER
 
-#define DEVICE_NAME_R                         "Sofle_R"                          /**< Name of device. Will be included in the advertising data. */
-#define DEVICE_NAME_L                        "Sofle_L"                          /**< Name of device. Will be included in the advertising data. */
-#define DEVICE_NAME_M                         "SofleBLE"                          /**< Name of device. Will be included in the advertising data. */
+#define KEYBOARD_SIDE SINGLE
+//#define KEYBOARD_SIDE RIGHT
+// CHANGE THIS FOR THE KEYBOARD TO MATCH WHAT IS BEING FLASHED. OPTIONS: LEFT  RIGHT  SINGLE
 
-#define DEVICE_MODEL                        "SofleBLE_V1"                          /**< Name of device. Will be included in the advertising data. */
+#define DEVICE_NAME_R                         "ErgoTravelBLE_R"                          /**< Name of device. Will be included in the advertising data. */
+#define DEVICE_NAME_L                        "ErgoTravelBLE_L"                          /**< Name of device. Will be included in the advertising data. */
+#define DEVICE_NAME_M                         "ErgoTravelBLE"                          /**< Name of device. Will be included in the advertising data. */
 
-#define MANUFACTURER_NAME                   "JosefAdamcik"                      /**< Manufacturer. Will be passed to Device Information Service. */
+#define DEVICE_MODEL                        "ErgoTravelBLE_V1"                          /**< Name of device. Will be included in the advertising data. */
+
+#define MANUFACTURER_NAME                   "JPConstantineau.com"                      /**< Manufacturer. Will be passed to Device Information Service. */
 
 
 #if KEYBOARD_SIDE == RIGHT
 #define KEYMAP( \
-  R00, R01, R02, R03, R04, R05,\
-  R10, R11, R12, R13, R14, R15, \
-  R20, R21, R22, R23, R24, R25,\
-  R30, R31, R32, R33, R34, R35,\
-  R40, R41, R42, R43, R44, R45\
-  ) \
-  { \
-    { R05, R04, R03, R02, R01, R00 }, \
-    { R15, R14, R13, R12, R11, R10 }, \
-    { R25, R24, R23, R22, R21, R20 }, \
-    { R35, R34, R33, R32, R31, R30 }, \
-    { R45, R44, R43, R42, R41, R40 } \
-  }
+      k00, k01, k02, k03, k04, k05, k06, \
+      k10, k11, k12, k13, k14, k15, k16, \
+      k20, k21, k22, k23, k24, k25, k26, \
+      k30, k31, k32, k33, k34, k35, k36 \
+) \
+{ \
+    { k06, k05, k04, k03, k02, k01, k00 }, \
+    { k16, k15, k14, k13, k12, k11, k10 }, \
+    { k26, k25, k24, k23, k22, k21, k20 }, \
+    { k36, k35, k34, k33, k32, k31, k30 } \
+}
 #else
 #define KEYMAP( \
-  L00, L01, L02, L03, L04, L05,\
-  L10, L11, L12, L13, L14, L15, \
-  L20, L21, L22, L23, L24, L25,\
-  L30, L31, L32, L33, L34, L35,\
-  L40, L41, L42, L43, L44, L45\
-  ) \
-  { \
-    { L00, L01, L02, L03, L04, L05 }, \
-    { L10, L11, L12, L13, L14, L15 }, \
-    { L20, L21, L22, L23, L24, L25 }, \
-    { L30, L31, L32, L33, L34, L35 }, \
-    { L40, L41, L42, L43, L44, L45 } \
-  } 
+      k00, k01, k02, k03, k04, k05, k06, \
+      k10, k11, k12, k13, k14, k15, k16, \
+      k20, k21, k22, k23, k24, k25, k26, \
+      k30, k31, k32, k33, k34, k35, k36 \
+) \
+{ \
+    { k00, k01, k02, k03, k04, k05, k06 }, \
+    { k10, k11, k12, k13, k14, k15, k16 }, \
+    { k20, k21, k22, k23, k24, k25, k26 }, \
+    { k30, k31, k32, k33, k34, k35, k36 } \
+} 
 
 #endif
 
--- firmware/keyboards/Malna_V3_old/hardware/pca10056/bluemicro840/hardware_config.h
+++ firmware/keyboards/Malna_V3_old/hardware/pca10056/bluemicro840/hardware_config.h
@@ -25,51 +25,28 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 6
-#define MATRIX_ROW_PINS { 13, 24, 9, 10, 38  }
-#define MATRIX_COL_PINS { 29, 2, 45, 3, 28, 43 }
 
-//  OLED DEFINITION
-    #define I2C_SDA_PIN 15
-    #define I2C_SCK_PIN 17
-    #define DISPLAY_U8G2_CONSTRUCTOR U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C // see https://github.com/olikraus/u8g2/wiki/u8g2setupcpp for reference
-
-// NOTE ONLY A SINGLE ENCODER IS POSSIBLE.
-#define ENCODER_PAD_A  26 
-#define ENCODER_PAD_B  30 
-#define ENCODER_RESOLUTION 2 // This doesn't do anything.
-
+#define MATRIX_ROW_PINS {13, 24, 9, 10, 38 }
+#define MATRIX_COL_PINS {29, 2, 45, 3, 28, 43 }
+#define UNUSED_PINS {}
+#define BACKLIGHT_LED_PIN 6 
+#define DEFAULT_PWM_VALUE 10000            // PWM intensity
+#define BACKLIGHT_PWM_ON 1  
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
-
-   #define BATTERY_TYPE BATT_LIPO
-        #define VBAT_PIN  4
+        #define BATTERY_TYPE BATT_LIPO
+        #define VBAT_PIN  31
         #define VCC_PIN 12
 #define VCC_POLARITY_ON 1
+#define  STATUS_BLE_LED_PIN  36  //blue = 0.19
+#define  STATUS_KB_LED_PIN 20  //red = 0.17
 
-	#define BACKLIGHT_PWM_ON 0
-	#define WS2812B_LED_PIN 33
-	
-	#define WS2812B_LED_COUNT 0
-	#define WS2812B_LED_ON 0 
-   /*     #define D3      6  
-        #define D2      8   
-        #define D1      15  
-        #define D0      17  
-        #define D4      20
-        #define C6      13
-        #define D7      24
-        #define E6      9
-        #define B4      10
-        #define B5      38 //1.06 = 32+6
+//encA = 30, encB = 26
 
-        #define F4      30
-        #define F5      26  
-        #define F6      29
-        #define F7      2
-        #define B1      45 //1.13  = 32+13
-        #define B3      3
-        #define B2      28
-        #define B6      43 //1.11 = 32+11
-        #define NC      32 //1.00 = 32+0 // NC is for not connected....*/
 
+    #define I2C_SDA_PIN 15
+    #define I2C_SCK_PIN 17
+	#define DISPLAY_U8G2_CONSTRUCTOR U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C // see https://github.com/olikraus/u8g2/wiki/u8g2setupcpp for reference
+
+    
 #endif /* HARDWARE_CONFIG_H */
