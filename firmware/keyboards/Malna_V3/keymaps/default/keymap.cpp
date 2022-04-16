66dc61d02a5adf7a9166197df09a12981c967d98
torjek <ntorjek@gmail.com>
2022-04-16 21:13:09 +0200

    Update keymap.cpp

--- firmware/keyboards/Malna_V3/keymaps/default/keymap.cpp
+++ firmware/keyboards/Malna_V3/keymaps/default/keymap.cpp
@@ -93,12 +93,11 @@ void setupKeymap() {
  */
     uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
         KEYMAP( \
-        KC_1,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_Y, \
-        KC_2,  KC_A,    KC_S,    KC_D,    KC_F,    KC_H, \
-        KC_3,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_N, \
-        KC_4,  KC_O,    KC_P,    KC_Q,    KC_R,    KC_S, \
-		KC_5,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0  \
-    );
+  KC_GRAVE,KC_1,KC_1,    KC_2,    KC_3,    KC_4,   KC_5, KC_UNDS,  \
+  KC_DEL  ,KC_F1,KC_1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  , KC_LCBR,   \
+  KC_LSFT ,KC_F7,KC_1  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 , KC_SPC,  \
+  KC_LCTL , KC_LGUI,KC_1, KC_LALT, LAYER_3, LAYER_1, KC_SPC,_______ \
+);
 
     /* Layer 2 (lower)
  * ,------------------------------------------------.
